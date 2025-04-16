0fdfe0d9493a1389a776e4cc30eea7720cb7fa2b
Thien0112 <128010271+Rebalon@users.noreply.github.com>
2025-04-16 18:52:14 +0700

    Test Run

--- src/Implementation/Classify.java
+++ src/Implementation/Classify.java
@@ -2,17 +2,21 @@ import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.classifiers.trees.J48;
 import weka.classifiers.Classifier;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.functions.LinearRegression;
 import weka.core.SerializationHelper;
 
 public class Classify {
     public static void main(String[] args) throws Exception {
         // Load training data
-        DataSource source = new DataSource("train_data.arff");
+        DataSource source = new DataSource("Test_DATA01.csv");
         Instances trainingData = source.getDataSet();
-        trainingData.setClassIndex(trainingData.numAttributes() - 1); // Set the class attribute
+        trainingData.setClassIndex(trainingData.attribute("all_percent").index()); // Set the class attribute
 
         // Train the classifier (J48)
-        Classifier classifier = new J48();
+        // Classifier classifier = new J48();
+        // classifier.buildClassifier(trainingData);
+        Classifier classifier = new LinearRegression();
         classifier.buildClassifier(trainingData);
 
         // Save the trained model
--- src/Implementation/PredictionExample.java
+++ src/Implementation/PredictionExample.java
@@ -5,19 +5,25 @@ import weka.core.converters.ConverterUtils.DataSource;
 
 public class PredictionExample {
     public static void main(String[] args) throws Exception {
+        // Disable SecurityManager
+        System.setSecurityManager(null); // Make sure to call this before anything else
+
         // Load the trained model
         Classifier classifier = (Classifier) weka.core.SerializationHelper.read("trained_model.model");
 
         // Load new data to predict
-        DataSource source = new DataSource("new_data.arff");
+        DataSource source = new DataSource("Test_DATA02.csv");
         Instances newData = source.getDataSet();
-        newData.setClassIndex(newData.numAttributes() - 1); // Set the class attribute
+        newData.setClassIndex(newData.numAttributes() - 1); // Set the class attribute (last column)
 
         // Make predictions on new instances
         for (int i = 0; i < newData.numInstances(); i++) {
             Instance currentInstance = newData.instance(i);
             double predictedClass = classifier.classifyInstance(currentInstance);
-            System.out.println("Predicted class for instance " + i + ": " + predictedClass);
+
+            // Convert predicted class to its string representation (for nominal classes)
+            String predictedClassName = newData.classAttribute().value((int) predictedClass);
+            System.out.println("Predicted class for instance " + i + ": " + predictedClassName);
         }
     }
 }
